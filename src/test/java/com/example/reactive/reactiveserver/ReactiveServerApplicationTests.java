a1f216e8e2cb7ede4eb3467ee6ec2571346e665f
kalana.w <abcd.1234>
2019-10-30 09:56:58 +0530

    first commit

---
+++ src/test/java/com/example/reactive/reactiveserver/ReactiveServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.reactive.reactiveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveServerApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
